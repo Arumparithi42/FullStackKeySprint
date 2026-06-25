5c6c64eb8862d7ca6bc76f6a2579838cc1d54401
Arumparithi42 <2024503542@student.annauniv.edu>
2026-06-25 15:11:10 +0530

    Added render deployment url

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://fullstackkeysprint.onrender.com/api';
 
 export async function registerUser({ username, email, password, repeatPassword }) {
   if (password !== repeatPassword) {
